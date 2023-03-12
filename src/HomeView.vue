2485fad69d3b68098a5951280e15b8f6a5f578f7
Eisen Saga <eisensaga15@gmail.com>
2023-03-13 01:20:28 +0800

    first commit

---
+++ src/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+
+	<!-- Main section start -->
+	<main-section></main-section>
+	<!-- Main section end -->
+
+	<!-- About section start -->
+	<about-section></about-section>
+	<!-- About section end -->
+
+	<!-- Services section start -->
+	<service-section></service-section>
+	<!-- Services section end -->
+
+	<!-- Portfolio section start -->
+	<portfolio-section></portfolio-section>
+	<!-- Portfolio section end -->
+
+
+	<!-- Contact section start -->
+	<contact-section></contact-section>
+	<!-- Contact section end -->
+
+</template>
+
+<script>
+import MainSection from '@/sections/MainSection.vue';
+import ServiceSection from '@/sections/ServiceSection.vue';
+import ContactSection from '@/sections/ContactSection.vue';
+
+export default {
+	components: {
+		MainSection,
+		ServiceSection,
+		ContactSection,
+	}
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/sections/ServiceSection.vue
@@ -0,0 +1,90 @@
+<template>
+    <!-- Services section start -->
+	<section id="services-section" class="px-12 lg:px-32 py-16 relative overflow-hidden">
+        <!-- Text wrapper start -->
+        <div class="flex gap-8 flex-col md:flex-row">
+            <!-- Main text start -->
+            <h1 class="uppercase text-5xl mb-4 font-semibold ">
+                OUR SERVICES
+            </h1>
+            <!-- Main text end -->
+        </div>
+        <!-- Text wrapper end -->
+
+        <!-- Services start -->
+        <div class="flex flex-col md:flex-row md:gap-8">
+            <!-- Wedding service start -->
+            <div class="flex flex-row md:flex-col justify-between md:justify-start items-center md:items-start my-8 md:w-1/3">
+                <img src="@/assets/services/wedding.png" alt="Wedding" class="-z-50 md:mb-4">
+                <div class="w-2/3 text-right md:text-left md:w-full">
+                    <h1 class="uppercase text-2xl font-semibold">
+                        Wedding
+                    </h1>
+                    <p class="capitalize text-stone-500">
+                        Users can also access information and fun facts about the landmark
+                        they are scanning, such as historical background, interesting facts,
+                        and tips for visiting. This combination of features can enhance the
+                        user's experience and provide an immersive and interactive way to
+                        explore famous landmarks.
+                    </p>
+                </div>
+            </div>
+            <!-- Wedding service end -->
+
+            <!-- Wedding service start -->
+            <div class="flex flex-row md:flex-col justify-between items-center md:items-start my-8 md:w-1/3">
+                <div class="w-2/3 text-left md:w-full md:order-2">
+                    <h1 class="uppercase text-2xl font-semibold">
+                        Lifestyle
+                    </h1>
+                    <p class="capitalize text-stone-500">
+                        Information and Fun Facts: In addition to the 3D model and AR
+                        features, the QR Code scanner can also provide users with
+                        information and fun facts about the landmark they are scanning.
+                        This feature can be particularly useful for tourists who are interested
+                        in learning more about the history and significance of the landmark
+                        they are visiting. The QR Code scanner can also provide users with
+                        tips on the best times to visit, the best spots to take photos, and
+                        other useful information that can enhance their overall experience.
+                    </p>
+                </div>
+            </div>
+            <!-- Wedding service end -->
+
+            <!-- Wedding service start -->
+            <div class="flex flex-row md:flex-col justify-between items-center md:items-start my-8 md:w-1/3">
+                <div class="w-3/3">
+                    <img src="@/assets/services/moments.png" alt="Moments" class="-z-50 md:mb-4">
+                </div>
+                <div class="w-2/3 text-right md:text-left md:w-full">
+                    <h1 class="uppercase text-2xl font-semibold">
+                        Moments
+                    </h1>
+                    <p class="capitalize text-stone-500">
+                        An option for text to speech is included for occasions where the user chooses
+                        to not read and instead listen to the information about the place. Information
+                        such as Historical Background, Interesting Facts, And Tips For Visiting. It is
+                        to maximize the immersion of visiting historical places here in Manila.
+                    </p>
+                </div>
+            </div>
+            <!-- Wedding service end -->
+        </div>
+        <!-- Services end -->
+
+        <!-- Circle start -->
+        <div class="bg-neutral-300 h-64 w-64 rounded-full absolute top-0 -right-20 mt-16 -z-20"></div>
+        <!-- Circle end -->
+	</section>
+	<!-- Services section end -->
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
